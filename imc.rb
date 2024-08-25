4900a030aae668ff0a3e0e51c0be4cece1d13e03
Coriolando <coriolando.medeiros@gmail.com>
2024-08-25 05:38:24 -0300

    feat: criado duas funções para calcular o IMC e dar saídas

--- imc.rb
+++ imc.rb
@@ -1,23 +1,53 @@
-puts "Preencha os dados a seguir"
-puts "Peso - kg"
-peso = gets.chomp.to_f
-puts "Altura - m"
-altura = gets.chomp.to_f
+# Tratamento de erros
+def entradas
+  puts "Nome"
+  @nome = gets.chomp.capitalize
 
-imc = peso / (altura * altura)
+  loop do
+    begin
+      puts "Peso - kg"
+      @peso = gets.chomp.gsub(',', '.') # Substitui vírgulas por pontos para a conversão
+      @peso = Float(@peso)  # Tenta converter a entrada para um número float
+      break  # Sai do loop se a conversão for bem-sucedida
+    rescue ArgumentError
+      puts "Erro: Isso não é um número válido. Por favor, tente novamente."
+    end
+  end
 
-if imc < 16.9
-   puts "Seu IMC é #{'%.2f' % imc}, magreza extrema"
-elsif (imc >17 ) and (imc < 18.5)
-   puts "Seu IMC é #{'%.2f' % imc}, abaixo do peso"
-elsif (imc > 18.4) and (imc < 25)
-   puts "Seu IMC é #{'%.2f' % imc}, peso normal"
-elsif (imc > 24.9) and (imc < 30)
-   puts "Seu IMC é #{'%.2f' % imc}, acima do peso"
-elsif (imc > 29.9) and (imc < 35)
-   puts "Seu IMC é #{'%.2f' % imc}, obesidade grau I"
-elsif (imc > 34.9) and (imc < 40.1)
-   puts "Seu IMC é #{'%.2f' % imc}, obesidade grau II - severa"
-elsif imc > 40
-   puts "Seu IMC é #{'%.2f' % imc}, obesidade grau III - morbida"
+  loop do
+    begin
+      puts "Altura - m"
+      @altura = gets.chomp.gsub(',', '.') # Substitui vírgulas por pontos para a conversão
+      @altura = Float(@altura)  # Tenta converter a entrada para um número float
+      break  # Sai do loop se a conversão for bem-sucedida
+    rescue ArgumentError
+      puts "Erro: Isso não é um número válido. Por favor, tente novamente."
+    end
+  end
+
+  # Cálculo do IMC
+  @imc = @peso / (@altura * @altura)
 end
+
+# Saídas da aplicação
+def saidas
+  if @imc < 16.9
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, magreza extrema!"
+  elsif (@imc >= 17) && (@imc < 18.5)
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, abaixo do peso!"
+  elsif (@imc >= 18.5) && (@imc < 25)
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, peso normal!"
+  elsif (@imc >= 25) && (@imc < 30)
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, acima do peso!"
+  elsif (@imc >= 30) && (@imc < 35)
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, obesidade grau I!"
+  elsif (@imc >= 35) && (@imc < 40)
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, obesidade grau II - severa!"
+  elsif @imc >= 40
+    puts "#{@nome}, seu IMC é #{'%.2f' % @imc}, obesidade grau III - mórbida!"
+  end
+end
+
+puts "Preencha os dados a seguir"
+entradas
+saidas
